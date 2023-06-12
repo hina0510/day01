131a0ca79170a9b7aa932e97213fb1792475ca68
ye yu jin <hina0510@naver.com>
2023-06-12 15:27:16 +0900

    플젝 생성

---
+++ src/day01/TestClass02.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class TestClass02 {
+	public static void main(String[] args) {
+		/*
+			escape : 문자열 안에서 특수한 기능을 한다.
+			\n : 엔터값과 비슷하다.
+			\t : tab크기만큼 오른쪽으로 커서 이동
+			\" : "(큰 따옴표) 하나 표현
+			\\ : \(역슬러시) 하나 표현
+		 */
+		System.out.println("안녕\n하세요");
+		System.out.println("\t1234567\t12345678\t1");
+		System.out.println("이름\t나이");
+		System.out.println("홍길동\t20살");
+		
+		System.out.println("	\"	\\	");
+		
+		
+	}
+}
---
+++ src/day01/TestClass03.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class TestClass03 {
+	public static void main(String[] args) {
+		System.out.println(100+100);
+		System.out.println(100.123+100.123);
+		
+		System.out.println("안녕"+"하세요");
+		System.out.println("100"+100);
+		System.out.println("백"+100);
+		
+		System.out.println("안녕"+100+300);
+		System.out.println("안녕"+(100+300));
+		// 문자열과 숫자를 연산 시 문자열로 출력됨. 숫자끼리 연산하고 싶으면 괄호를 사용
+	}
+}
